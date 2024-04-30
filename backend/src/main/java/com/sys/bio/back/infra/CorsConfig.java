5660ce49e661072cd6da46c2930a37ea3c8265f3
ignacio <ignacioaalvarezc@gmail.com>
2024-04-30 17:20:39 -0400

    Primer commit

---
+++ backend/src/main/java/com/sys/bio/back/infra/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.sys.bio.back.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        //config.addAllowedOrigin("http://18.119.134.114");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ backend/src/main/java/com/sys/bio/back/models/reception/Reception.java
@@ -0,0 +1,35 @@
+package com.sys.bio.back.models.reception;
+
+import com.sys.bio.back.models.user.Responsible;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "receptions")
+public class Reception {
+
+    @Id
+    private Long receptionId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Responsible responsible;
+    private Integer acceptedBales;
+    private Integer rejectedBales;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Double totalHours;
+    private String reasonRejected;
+    private LocalDate filterDate;
+
+}
