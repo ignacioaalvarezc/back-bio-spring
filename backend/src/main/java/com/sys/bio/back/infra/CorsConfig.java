0381f19ba3c699c0fe9bc507f72c59ec4f0ded35
ignacio <ignacioaalvarezc@gmail.com>
2024-05-10 11:54:15 -0400

    Update

--- backend/src/main/java/com/sys/bio/back/infra/CorsConfig.java
+++ backend/src/main/java/com/sys/bio/back/infra/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         //config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://vps-4088220-x.dattaweb.com");
-        config.addAllowedOrigin("http://localhost://149.50.140.114");
+        config.addAllowedOrigin("http://149.50.140.114");
         config.addAllowedOrigin("https://softbio.cl");
         //config.addAllowedOrigin("http://18.119.134.114");
         config.addAllowedHeader("*");
